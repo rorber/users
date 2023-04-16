be9b69c9a394f03fc723052f903979247df28802
r <19149315+rorber@users.noreply.github.com>
2023-04-16 21:56:25 +1000

    make the header logo a link

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { UsersTable } from "./components/UsersTable/UsersTable";
 import { QueryClient, QueryClientProvider } from "react-query";
 
+const BEELINE_HOMEPAGE_URL = `https://makeabeeline.com`;
+
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
@@ -15,14 +17,16 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <div>
-        <header className="px-4 py-12 bg-red">
-          <div className="max-w-content m-auto">
-            <img
-              alt="Beeline logo"
-              height="40px"
-              src="logo.png"
-              width="130px"
-            ></img>
+        <header className="py-12 bg-red">
+          <div className="max-w-content m-auto px-4">
+            <a href={BEELINE_HOMEPAGE_URL} rel="noreferrer" target="_blank">
+              <img
+                alt="Beeline logo"
+                height="40px"
+                src="logo.png"
+                width="130px"
+              ></img>
+            </a>
           </div>
         </header>
         <main className="max-w-content mx-auto my-20 px-4">
